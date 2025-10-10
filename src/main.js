8f9fb0e5e85c6f198c52f7b7eba18dbaa12d78cb
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-10-10 10:34:23 +0000

    ajout de la feuille de style Font Awesome pour les icônes

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import "vue3-toastify/dist/index.css";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 import BootstrapVue3 from 'bootstrap-vue-3'
 
